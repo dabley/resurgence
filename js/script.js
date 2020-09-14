59e8f68cbe647c312b08401ed8ef53c6748e9f27
Tyeth Gundry <tyethgundry@goolemail.com>
2020-09-14 14:11:49 +0100

    Events CSS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log("sc");
+console.log("front page event script loaded, painting events");
+jQuery.each(document.getElementsByClassName("node--type-event"),function(elementIndex,eventElement) {
+            var eventColourClass = 'r_events';
+            var blockEventType = eventElement.getElementsByClassName('block-field-blocknodeeventfield-event-type')[0];
+            var blockEventTitle = eventElement.getElementsByClassName('block-field-blocknodeeventtitle')[0];
+            var eventElementType = blockEventType.getElementsByClassName('field__item')[0].innerText
+            switch (eventElementType.toLowerCase()) {
+                case ('community'):
+                    eventColourClass = 'r_community';
+                    break;
+                case ('live'):
+                    eventColourClass = 'r_live';
+                    break;
+                case ('talks'):
+                case ('talk'):
+                    eventColourClass = 'r_talks';
+                    break;
+                case ('courses'):
+                case ('course'):
+                    eventColourClass = 'r_courses';
+                    break;
+                default:
+                    break;
+            }
+            blockEventTitle.classList.add(eventColourClass);
+            blockEventType.classList.add(eventColourClass);
+            var linkElement = eventElement.getElementsByClassName('field--name-field-link-to-event-page')[0].getElementsByTagName('A')[0];
+            linkElement.innerText = "Find out more...";
+        });
+        
