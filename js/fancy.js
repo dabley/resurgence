082b227217d8aba82435a83fe401ccf249343ef0
Tyeth Gundry <tyethgundry@goolemail.com>
2020-10-08 12:09:27 +0100

    Add instructions for additional library JS script

--- js/fancy.js
+++ js/fancy.js
@@ -1,1 +1,6 @@
-console.log("fancy");
+/* Implemented in Resurgence theme to facilitate a single page having additional javascript libraries. */
+/* If no longer needed then remove entries from resurgence.libraries.yml and resurgence.theme files.   */
+console.log(
+    "Front page specific javascript code, to implement autoplaying and stopping video," +  
+    " to be replaced with image placeholder before and after play. Cookie???"
+    );
